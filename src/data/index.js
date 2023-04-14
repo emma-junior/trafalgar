816d910fe5c28797ec0f7e1bb9262710f16dc161
emma-junior <emnzboy31@gmail.com>
2023-04-14 20:25:04 +0100

    created data folder

---
+++ src/data/index.js
@@ -0,0 +1,50 @@
+import searchDoctor from "../images/Frame.svg";
+import onlinePharm from "../images/drug.svg";
+import consultation from "../images/phone.svg";
+import detailsInfo from "../images/detailsInfo.svg";
+import emergencyCare from "../images/emergency.svg";
+import tracking from "../images/tracking.svg";
+
+export const services = [
+  {
+    id: 1,
+    icon: searchDoctor,
+    title: "Search Doctor",
+    content:
+      "Choose your doctor from thousands of specialist, general, and trusted hospitals",
+  },
+  {
+    id: 2,
+    icon: onlinePharm,
+    title: "Online Pharmacy",
+    content:
+      "Buy your medicines with our mobile application with a simple delivery system",
+  },
+  {
+    id: 3,
+    icon: consultation,
+    title: "Consultation",
+    content:
+      "Free consultation with our trusted doctors and get the best recommendations",
+  },
+  {
+    id: 4,
+    icon: detailsInfo,
+    title: "Details info",
+    content:
+      "Free consultation with our trusted doctors and get the best recommendations",
+  },
+  {
+    id: 5,
+    icon: emergencyCare,
+    title: "Emergency care",
+    content:
+      "You can get 24/7 urgent care for yourself or your children and your lovely family",
+  },
+  {
+    id: 6,
+    icon: tracking,
+    title: "Tracking",
+    content: "Track and save your medical history and health data",
+  },
+];
